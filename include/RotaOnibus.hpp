a776ddbcd72a26a0367faa2754576039b11af234
PedroHCCo <pedrohc.coelho@gmail.com>
2026-04-07 18:09:03 -0300

    Update RotaOnibus.hpp

--- include/RotaOnibus.hpp
+++ include/RotaOnibus.hpp
@@ -1,1 +1,34 @@
+#ifndef ROTAONIBUS_HPP
+#define ROTAONIBUS_HPP
 
+#include <string>
+
+struct Parada {
+    std::string nome;
+    Parada* proximo;
+    Parada* anterior;
+    
+    Parada(std::string n);
+};
+
+class RotaOnibus {
+private:
+    Parada* cabeca;
+    Parada* cauda;
+    int total;
+
+public:
+    RotaOnibus();
+    ~RotaOnibus();
+    void inserirInicio(std::string nome);
+    void inserirFim(std::string nome);
+    void inserirPorPosicao(std::string nome, int posicao);
+    void removerPrimeira();
+    void removerUltima();
+    void buscarPorNome(std::string busca);
+    void listarIda();
+    void listarVolta();
+    void mostrarTotal();
+};
+
+#endif
